219209d5bf461df249193e7079b842ec8d362163
Jess-chege <biankachege98@gmail.com>
2021-10-08 22:27:28 +0300

    push to the repo

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ import Logger from './config/logger';
 const app = express();
 global.logger = Logger.createLogger({ label: 'AFRICAFORYOU' });
 
+app.use(express.json());
+app.use(routes);
+
+app.use((err, req, res, next) => {
+    err.statusCode = er.statusCode ||500;
+    err.message = err.message || "server error";
+    res.status(err.statusCode).json({
+        message: err.message,
+    });
+});
 appConfig(app);
 
 export default app;
